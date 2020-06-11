3c0dbb2db710d571c5e570652da7ec152148b47a
chunghyun <chung629@naver.com>
2020-06-11 16:29:33 +0900

    intro finish

--- app/src/main/java/com/example/kakaoeventttsapplication/SmsActivity.java
+++ app/src/main/java/com/example/kakaoeventttsapplication/SmsActivity.java
@@ -2,6 +2,8 @@ package com.example.kakaoeventttsapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
@@ -10,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SmsActivity extends AppCompatActivity {
    EditText editTextSend;
    EditText editTextContent;
-
+   Button button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,14 @@ public class SmsActivity extends AppCompatActivity {
         editTextSend = findViewById(R.id.senderText);
         Intent passedIntent = getIntent();
         processIntent(passedIntent);
+
+        button = findViewById(R.id.close);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
     private void processIntent(Intent intent){
         if(intent != null){
