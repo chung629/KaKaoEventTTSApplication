010b95380958de55da65529f8f6464b910c71aa7
chunghyun <chung629@naver.com>
2020-08-17 22:15:05 +0900

    1. 알림권한 설정 관련 작성 완료 Manifests 수정 및 Gradle 수정 했습니다(메소드 특정 수 이상으로 사용할 수 있도록 multiDexEnabled true로 추가함)

--- app/src/main/java/com/example/kakaoeventttsapplication/MainActivity.java
+++ app/src/main/java/com/example/kakaoeventttsapplication/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.kakaoeventttsapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -16,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     // 기능사용하기, 알림 버튼
     Switch FuncSwitch;
     Switch NoticeSoundSwitch;
+
+    // 알림 안내 권한 설정
+    Switch Alarm_Permission_Switch;
+    Button Alarm_Permission_Setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +41,23 @@ public class MainActivity extends AppCompatActivity {
         NoticeSoundSwitch.setOnCheckedChangeListener(new FuncAndNoticeSettings.NoticeSwitchListener());
         // 기능 사용하기 및 알림 설정 끝
 
-
+        // 알림 안내 권한 설정
+        Alarm_Permission_Switch = findViewById(R.id.permission_state_switch);
+        Alarm_Permission_Setting = findViewById(R.id.alarm_permission_setting);
+        Alarm_Permission_Setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent armSet = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+                armSet.addCategory(Intent.CATEGORY_DEFAULT);
+                armSet.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivityForResult(armSet, 0);
+            }
+        }); // 앱 상세 설정 화면으로 이동
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS))
+            Alarm_Permission_Switch.setChecked(false);
+        else
+            Alarm_Permission_Switch.setChecked(true);
+        // 알림 안내 권한 설정 끝
 
         // SMS 수신 허가
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
@@ -38,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "SMS 수신권한을 사용자가 승인함", Toast.LENGTH_SHORT).show();
         }else{
             Toast.makeText(getApplicationContext(), "SMS 수신권한을 사용자가 거부함", Toast.LENGTH_SHORT).show();
-
             if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)){
                 Toast.makeText(getApplicationContext(), "SMS권한이 필요합니다", Toast.LENGTH_SHORT).show();
                 ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.RECEIVE_SMS}, SMS_RECEIVE_PERMISSON);
@@ -63,4 +90,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     // SMS 수신 허가 관련
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //requestCode = 0는 알림 안내 권한 설정 관련 코드 입니다
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 알림 권한 설정 관련
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)) {
+            Alarm_Permission_Switch.setChecked(false);
+        } else {
+            Alarm_Permission_Switch.setChecked(true);
+        }
+    }
 }
